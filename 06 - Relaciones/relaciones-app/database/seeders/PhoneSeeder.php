220bbe20b38d4c6331e29b6aec87b1c4a9f03b37
Ramirez-05 <ramirezcardona2204@gmail.com>
2024-03-18 11:26:55 -0500

    Cambios realizados en el código

---
+++ 06 - Relaciones/relaciones-app/database/seeders/PhoneSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Phone;
+
+class PhoneSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Phone::create([
+            'prefix' => 34,
+            'phone_number' =>  '6666',
+            'user_id' => 1
+        ]);
+
+        Phone::create([
+            'prefix' => 33,
+            'phone_number' =>  '7777',
+            'user_id' => 1
+        ]);
+
+        Phone::create([
+            'prefix' => 32,
+            'phone_number' =>  '8888',
+            'user_id' => 1
+        ]);
+    }
+}
